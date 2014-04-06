d1c1ac29c0ccdd7135f165bb81ef96764d4cd532
NCrashed <NCrashed@gmail.com>
2014-04-06 20:22:41 +0400

    Docs generator settings

---
+++ generate.sh
@@ -0,0 +1,6 @@
+rdmd bootDoc/generate.d ../source \
+-I~/.dub/packages/vibe-d-master/source \
+-I~/.dub/packages/openssl-master \
+-I~/.dub/packages/derelict-pq-master/source \
+-I~/.dub/packages/derelict-util-1.0.0/source \
+-I~/.dub/packages/dunit-1.0.8/source
--- index.d
+++ index.d
@@ -1,28 +1,6 @@
-dlogg
-=====
-
-[![Build Status](https://travis-ci.org/NCrashed/dlogg.svg?branch=master)](https://travis-ci.org/NCrashed/dlogg)
-
-Library that consilidates logging utilites according following goals:
-* Scalability
-* Nothrow and stable work under high loads and concurrency
-* Working with daemons
-* Log rotating 
-* Lazy logging including buffered loggers
-
-Installing
-===========
-Add following in your dub package file:
-```Json
-"dependencies": {
-  "dlogg": ">=0.1.0"
-}
-```
-
-Examples:
-========
+/**
 Each logger in the package implements `dlogg.ILogger` interface and forced to be shared between threads. There are 5 different styles of logg messages:
-```D
+=========
 enum LoggingLevel
 {
     Notice,
@@ -31,14 +9,15 @@ enum LoggingLevel
     Fatal,
     Muted // Used only to mute all writing to console
 }
-```
+==========
 
 So, to put message in log you can use:
-```D
+==========
   void ILogger.log(lazy string message, LoggingLevel level) shared;
-```
+==========
+
 Or one of handy wrappers:
-```D
+==========
   final nothrow shared
   {
     void logDebug(E...)(E args) shared @trusted; // not lazy
@@ -46,39 +25,41 @@ Or one of handy wrappers:
     void ILogger.logWarning(lazy string message);
     void ILogger.logError(lazy string message);
   }
-```
+==========
 Note that major logging funcs are lazy and we don't have overhead while logg call isn't needed but in strict version D performs string concatinations.
 
 And finally full example:
-```D
+=============
    shared ILogger logger = new StrictLogger("my_awesome_log.log");
    logger.minOutputLevel = LoggingLevel.Warning; // info msgs won't be printed in console 
    logger.logInfo("Info message!");
    logger.logError("Error message!");
    logger.logDebug("Debug message!");
-```
+=============
 Output file:
-```
+=============
 [2014-04-06T19:50:22.8193333]:Notice: Notice msg!
 [2014-04-06T19:50:22.819406]:Warning: Warning msg!
 [2014-04-06T19:50:22.8194323]:Debug: Debug msg!
 [2014-04-06T19:50:22.8194548]:Error: Fatal msg!
-```
+=============
 
-`minOutputLevel` property controls which message types should be printed in console. `LoggingLevel.Muted` is used to create daemon loggers that don't try to write into stdout.
+$(B minOutputLevel) property controls which message types should be printed in console. `LoggingLevel.Muted` is used to create daemon loggers that don't try to write into stdout.
 
 Buffered logger
 ===============
+
 In many cases (for instance, unittesting) we don't want log output just in time. Buffered logger adds control when messages would be written into wrapped log. Example:
-```D
+==============
    auto delayed = new shared BufferedLogger(logger); // wrapping a logger
    scope(exit) delayed.finalize(); // write down information in wrapped logger
    scope(failure) delayed.minOutputLevel = LoggingLevel.Notice; // if failed, spam in console
    delayed.logNotice("Hello!");
 
    // do something that can fail
-```
+==============
 
 Log rotating
 ============
-`StrictLogger.reload` function checks if the log file is exists at specified location and if can't find it, recreates the file and continues write into it. Useful for [logrotate](http://linuxcommand.org/man_pages/logrotate8.html) utility. GNU/Linux system checks file identity by inode, that doesn't change while renaming. Thus after renaming the file at location log continues write into the renamed file. The call to the reload method force splitting log into two parts.
+$(B StrictLogger.reload) function checks if the log file is exists at specified location and if can't find it, recreates the file and continues write into it. Useful for <a href=http://linuxcommand.org/man_pages/logrotate8.html>logrotate</a> utility. GNU/Linux system checks file identity by inode, that doesn't change while renaming. Thus after renaming the file at location log continues write into the renamed file. The call to the reload method force splitting log into two parts.
+*/
